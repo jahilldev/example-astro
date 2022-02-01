a4cca51fd4049fab32e51904ba3476247f9cfae8
James Hill <jahill@groupon.com>
2022-02-01 13:08:00 +0000

    Added preact parser and example hydration

---
+++ src/components/button.tsx
@@ -0,0 +1,41 @@
+import { h } from 'preact'
+import { useState } from 'preact/hooks';
+
+/* -----------------------------------
+ *
+ * IProps
+ *
+ * -------------------------------- */
+
+interface IProps {
+   className?: string;
+   children?: any;
+}
+
+/* -----------------------------------
+ *
+ * Button
+ *
+ * -------------------------------- */
+
+function Button({ className, children }: IProps) {
+   const [isActive, setIsActive ] = useState(false);
+
+   return (
+      <button class={className} onClick={onClick}>{children}</button>
+   );
+
+   function onClick() {
+      setIsActive(!isActive);
+
+      console.log('Button.onClick', { isActive });
+   }
+}
+
+/* -----------------------------------
+ *
+ * Export
+ *
+ * -------------------------------- */
+
+export { Button };
